e4802e283fcb33c698ac7a09258a9a2680ea15c9
Bishesh Shrestha <bisheshshr@gmail.com>
2024-03-27 18:19:56 -0500

    update to user table schema

--- BackEnd/schema.sql
+++ BackEnd/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
+    user_id integer primary key,
     username varchar(255) NOT NULL,
     email varchar(255) NOT NULL UNIQUE,
     password varchar(255) NOT NULL
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const sqlite = require('better-sqlite3')
+const path = require('path')
 const app = express()
 const PORT = 8080
+
+app.use(express.json())
+
+
+app.post("/Login", (req, res) => {
+    if (req.body.username === undefined || req.body.password === undefined) {
+        res.status(400).send("invalid username or password");
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log(`server now live on ${PORT}`)
