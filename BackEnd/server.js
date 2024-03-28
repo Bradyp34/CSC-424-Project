70a703bc2e6addf4022f9e7d25acff63f1b711b7
Bishesh Shrestha <bisheshshr@gmail.com>
2024-03-28 15:03:49 -0500

    Added some error handling functionalitites

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,19 +1,54 @@
 const express = require('express');
 const sqlite = require('better-sqlite3');
 const path = require('path');
+const winston = require('winston'); //Error Handling
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.post("/Login", (req, res) => {
-    if (req.body.username === undefined || req.body.password === undefined) {
-        res.status(400).send("invalid username or password");
+//Custom error class we could add more in the future use this
+class BadRequestError extends Error{
+    constructor(message){
+        super(message);
+        this.name = 'BadRequestError';
+        this.status = 400;
     }
+}
+
+//Error handling middleware
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).json({error: 'Internal Server Error'});
 });
+//This creates an error log file called 
+const logger = winston.createLogger({
+    level: 'error',
+    format: winston.format.json(),
+    transports:[
+        new winston.transports.File({filename: 'error.log'})
+    ]
+});
+//Logging the middleware
+app.use((err, req, res, next) => {
+    logger.error(err.message, { error: err.stack });
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello, world!');
+});
+
+app.post("/Login", (req, res) => {
+    try{
+        if (req.body.username === undefined || req.body.password === undefined) {
+            throw new BadRequestError('Invalid username or password');
+        } 
+    } catch(error){
+        next(error);
+    }
 });
 
 app.listen(PORT, () => {
