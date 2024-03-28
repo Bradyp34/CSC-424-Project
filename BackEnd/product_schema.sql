e17236f57be787475d1bf3a6019f0da7534877f3
Bishesh Shrestha <bisheshshr@gmail.com>
2024-03-28 15:05:37 -0500

    Changed the logic on product_status, now only accepts certain values

--- BackEnd/product_schema.sql
+++ BackEnd/product_schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE products (
     product_name VARCHAR(255) NOT NULL,
     product_type VARCHAR(255) NOT NULL,
     product_location VARCHAR(255) NOT NULL,
-    total_product_count INTEGER NOT NULL CHECK (total_product_count >= 0), -- Ensure total count is non-negative
+    total_product_count INTEGER NOT NULL CHECK (total_product_count >= 0),-- Ensure total count is non-negative
     product_status VARCHAR(255) NOT NULL DEFAULT 'available' CHECK (product_status IN ('available', 'sold', 'on-hold')), -- Allow only specific values
     product_sale_count INTEGER NOT NULL CHECK (product_sale_count >= 0), -- Ensure sale count is non-negative
     product_on_hold_count INTEGER NOT NULL CHECK (product_on_hold_count >= 0) -- Ensure on hold count is non-negative
