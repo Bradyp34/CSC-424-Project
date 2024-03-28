6c21cfc4f6f21252071be4c0442b1b0f85a9e9d5
Bishesh Shrestha <bisheshshr@gmail.com>
2024-03-28 00:10:01 -0500

    Changed the size of some attributes

--- BackEnd/product_schema.sql
+++ BackEnd/product_schema.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS products;
 
 CREATE TABLE products (
     product_id integer PRIMARY KEY,
-    product_name varchar(50) NOT NULL,
-    product_type varchar(20) NOT NULL,
-    product_location varchar(50) NOT NULL, -- Corrected column name and added NOT NULL
+    product_name varchar(255) NOT NULL,
+    product_type varchar(255) NOT NULL,
+    product_location varchar(255) NOT NULL, -- Corrected column name and added NOT NULL
     total_product_count integer NOT NULL,
-    product_status varchar(20) DEFAULT 'available' NOT NULL --Default is 'available' can change it to 'sold' and 'on-hold'
+    product_status varchar(255) DEFAULT 'available' NOT NULL --Default is 'available' can change it to 'sold' and 'on-hold'
     product_sale_count integer NOT NULL,
     product_on_hold_count integer NOT NULL --come up with a better name someone
 );
