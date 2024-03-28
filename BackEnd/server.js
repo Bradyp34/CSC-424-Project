dcf0ccc858afdbb771405aec100296454f47a03a
Bishesh Shrestha <bisheshshr@gmail.com>
2024-03-28 15:54:07 -0500

    Made some change sto the structure of the code

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,60 +1,33 @@
 const express = require('express');
 const sqlite = require('better-sqlite3');
 const path = require('path');
-const winston = require('winston'); //Error Handling
+const { errorHandlerMiddleware, loggingMiddleware, BadRequestError } = require('./errorHandlers'); //Error Handling
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-//Custom error class we could add more in the future use this
-class BadRequestError extends Error{
-    constructor(message){
-        super(message);
-        this.name = 'BadRequestError';
-        this.status = 400;
-    }
-}
-
-//Error handling middleware
-app.use((err, req, res, next)=>{
-    console.error(err.stack);
-    res.status(500).json({error: 'Internal Server Error'});
-});
-//This creates an error log file called 
-const logger = winston.createLogger({
-    level: 'error',
-    format: winston.format.json(),
-    transports:[
-        new winston.transports.File({filename: 'error.log'})
-    ]
-});
-//Logging the middleware
-app.use((err, req, res, next) => {
-    logger.error(err.message, { error: err.stack });
-    res.status(500).json({ error: 'Internal Server Error' });
-});
-
-
-
 app.get('/', (req, res) => {
     res.status(200).send('Hello, world!');
 });
 
-app.post("/Login", (req, res) => {
-    try{
+app.post("/Login", (req, res, next) => {
+    try {
         if (req.body.username === undefined || req.body.password === undefined) {
             throw new BadRequestError('Invalid username or password');
-        } 
-    } catch(error){
+        }
+    } catch (error) {
         next(error);
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`server now live on ${PORT}`);
-});
+// Error handling middleware
+app.use(errorHandlerMiddleware);
+app.use(loggingMiddleware);
 
+app.listen(PORT, () => {
+    console.log(`Server now live on ${PORT}`);
+});
 
 module.exports = { app };
 
