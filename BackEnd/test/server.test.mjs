670c8c3bfc4ee1b8e4ec833abfe4770c2a9380e6
Bishesh Shrestha <bisheshshr@gmail.com>
2024-03-28 00:08:23 -0500

    Changed the way we import the 'chai' library everything else is same

--- BackEnd/test/server.test.mjs
+++ BackEnd/test/server.test.mjs
@@ -1,13 +1,8 @@
-import {chai} from 'chai';
-import {chaiHttp} from 'chai-http';
-import {pkg} from '../server.js';
-const { app } = pkg;
-// import app from '../server.js'; // Ensure the path is correct and '.js' extension is specified
+import { expect, use } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../server.js'; // Ensure the path is correct and '.js' extension is specified
 
-const { expect } = chai;
-chai.use(chaiHttp);
-
-
+use(chaiHttp);
 
 describe('Server', () => {
   it('should return 200 OK status', (done) => {
