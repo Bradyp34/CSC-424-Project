43ae9e2c0c63aa385eeb2dee3baaf6b6d0782ee8
ismailraji <ismailraji53@gmail.com>
2024-03-31 15:58:59 -0500

    added an activity log for everytime the server is started

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -10,6 +10,8 @@ const app = express();
 const fs = require("fs");
 const { debugPort } = require("process");
 const PORT = 3000;
+const activity_log_file = "log.txt";
+const current_time = new Date()
 
 const db = sqlite("database.db");
 const file_date = fs.readFileSync(
@@ -23,17 +25,33 @@ app.use(errorHandlerMiddleware);
 app.use(loggingMiddleware);
 
 app.get("/", (req, res) => {
+    const log = `Server live on port ${PORT}. At ${current_time}\n`
+    fs.appendFile(activity_log_file, log, (err) => {
+        if (err) {
+            console.log(err)
+            process.exit(0)
+        }
+        else 
+        {
+            console.log("successfully logged")
+        }
+    })
   res.status(200).send("Server now running");
 });
 
-app.get('/all_users', async (req, res) => {
-    const statement = db.prepare('select * from users').all()
-    res.status(200).send(statement)
-})
+app.get("/all_users", async (req, res) => {
+  const statement = db.prepare("select * from users").all();
+  res.status(200).send(statement);
+});
 
 app.post("/Register", async (req, res) => {
-  const { username, email, password , user_type} = req.body;
-  if (username === undefined || email === undefined || password === undefined || user_type === undefined) {
+  const { username, email, password, user_type } = req.body;
+  if (
+    username === undefined ||
+    email === undefined ||
+    password === undefined ||
+    user_type === undefined
+  ) {
     res.status(400).send("Missing fields / parameters");
     return;
   }
@@ -43,28 +61,23 @@ app.post("/Register", async (req, res) => {
     return;
   }
 
-  if (user_type !== "admin")
-  {
-    res.status(400).send("Invalid User Type")
-    return
+  if (user_type !== "admin") {
+    res.status(400).send("Invalid User Type");
+    return;
   }
-  try{
+  try {
     const statement = db.prepare(
-        "insert into users(username, user_type, email, user_password) values(?, ?, ? ,?)"
-      );
-      const user_type = "admin"
-      const response = statement.run(username, user_type, email,  password);
+      "insert into users(username, user_type, email, user_password) values(?, ?, ? ,?)"
+    );
+    const user_type = "admin";
+    const response = statement.run(username, user_type, email, password);
     //   console.log("Account Creation sucessful")
-      res.status(201).send('Account Creation Sucessful');
+    res.status(201).send("Account Creation Sucessful");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal server error");
   }
-  catch (error)
-  {
-    console.log(error)
-    res.status(500).send("Internal server error")
-  }
-  
 });
-
 
 app.post("/Login", async (req, res) => {
   if (req.body.username === undefined || req.body.password === undefined) {
