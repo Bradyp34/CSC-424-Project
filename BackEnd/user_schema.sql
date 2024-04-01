387511bb4ea0516b97719683f8fe0f2e0179ae4c
Bishesh Shrestha <bisheshshr@gmail.com>
2024-03-31 22:15:50 -0500

    Did some refactoring and made changes accordingly everywhere else

--- BackEnd/user_schema.sql
+++ BackEnd/user_schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     user_id integer primary key,
     username varchar(255) NOT NULL UNIQUE,
-    user_type varchar(50) NOT NULL DEFAULT 'non-admin' CHECK(user_type IN ('non-admin', 'admin')),
+    user_type varchar(50) NOT NULL DEFAULT 'regular' CHECK(user_type IN ('regular', 'admin')), --Changed to 'regular'
     email varchar(255) NOT NULL UNIQUE,
     user_password varchar(255) NOT NULL
 );
