ab466d01419b7c2dcb102d5863a74897c02fb763
HotBunny <ehmoo924@gmail.com>
2024-04-01 21:47:30 -0500

    Add login and registration pages
    - Updated package-lock.json and package.json with necessary dependencies.
    - Modified App.jsx to include new authentication routes.
    - Enhanced Login component with improved functionality.
    - Added styling for login page in Login.css.
    - Implemented new Login page component.
    - Created new Register.css for registration page styling.
    - Developed new Register page component for user registration.

--- FrontEnd/my-app/src/App.jsx
+++ FrontEnd/my-app/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Login></Login>
+      <Register></Register>
     </div>
   );
 }
--- FrontEnd/my-app/src/Components/Login.jsx
+++ FrontEnd/my-app/src/Components/Login.jsx
@@ -5,6 +5,7 @@ class Login extends Component {
     return (
       <div>
         
+        
       </div>
     )
   }
