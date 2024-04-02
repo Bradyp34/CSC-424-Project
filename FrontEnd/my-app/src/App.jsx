a6284b2a1131dcb88d84e5cf9c378506ab8d606e
Bradyp34 <Brady.Petterson@trojans.dsu.edu>
2024-04-02 15:13:28 -0500

    commented out imports on app.jsx

--- FrontEnd/my-app/src/App.jsx
+++ FrontEnd/my-app/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
-import Login from "./pages/login/Login";
+/*import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-
+*/
 
 function App() {
   return (
