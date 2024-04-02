d9a5f36a6507ac76d0964d4c7c065ad2cc299e62
Bishesh Shrestha <bisheshshr@gmail.com>
2024-04-02 16:41:03 -0500

    Removed Comments and debug statements

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -11,7 +11,7 @@ const fs = require("fs");
 const { debugPort } = require("process");
 const { use } = import("chai");
 const PORT = 3000;
-const activity_log_file = "log.log";
+const activity_log_file = "log.txt";
 const current_time = new Date();
 
 const db = sqlite("database.db");
@@ -52,21 +52,9 @@ app.get("/all_users", async (req, res) => {
   res.status(200).send(statement);
 });
 
-// async function isValidAdmin(adminCredentials){
-//   try {
-//     const {username, email, password} = adminCredentials;
-//     console.log("Validating admin credentials:", adminCredentials);// Debugging purposes
-//     const admin = await db.prepare("select * from users where username = ? and email = ? and password = ? and user_type = 'admin'").get(username, email, password);
-//     console.log("Admin found:", admin);// Debugging purposes
-//     return admin != undefined;
-//   } catch (error) {
-//     console.error("Error validating admin credentials:", error);
-//     return false;
-//   }
-// }
 
 app.post("/Register", async (req, res) => {
-  const adminInfo = req.headers["admin-info"];
+
   const { username, email, password, user_type } = req.body;
   if (
     username === undefined ||
@@ -88,13 +76,13 @@ app.post("/Register", async (req, res) => {
     return;
   } 
 
-    const repeated_username = db.prepare("select * from users where username = ?").get(username);
-    if(repeated_username !== undefined ){
+    const repeated_username = db.prepare("select * from users where username = ?").all(username);
+    if(repeated_username.length > 0){
       res.status(400).send("Username already exists");
       return;
     }
-    const repeated_email = db.prepare("select * from users where email = ?").get(email);
-    if(repeated_email !== undefined){
+    const repeated_email = db.prepare("select * from users where email = ?").all(email);
+    if(repeated_email.length !== 0){
       res.status(400).send("Account with the email provided already exists");
       return;
     }
@@ -102,9 +90,7 @@ app.post("/Register", async (req, res) => {
       const statement = db.prepare(
         "insert into users(username, user_type, email, password) values(?, ?, ? ,?)"
       );
-     // const user_type = "admin";
       const response = statement.run(username, user_type, email, password);
-      //   console.log("Account Creation sucessful")
       return res.status(201).send("Account Creation Sucessful");
     } catch (error) {
       console.log(error);
