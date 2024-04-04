e3c7e885c83b6f39b9f12e9d8937efc2bbf3198e
Ray Yang <raylol9413@outlook.com>
2024-04-04 18:05:41 -0500

    Setting up the Inventory Page

--- FrontEnd/my-app/src/Pages/InventoryPage.jsx
+++ FrontEnd/my-app/src/Pages/InventoryPage.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import DataFetching from '../Components/DataFetching'
 
 function InventoryPage() {
   return (
     <div>
       <Navbar></Navbar>
-      <div className='text-white'>Inventory Page</div>
+      
+      <div className='text-black flex-col h-screen mt-[50px] bg-gray-900'>
+        <div className='h-[100%] border-2 rounded-2xl p-8 m-[100px] justify-center bg-white'>
+            <table>
+              <DataFetching></DataFetching>
+            </table>
+        </div>
+      </div>
+      
     </div>
   )
 }
