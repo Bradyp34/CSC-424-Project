2dd58e3803c5d854d730331c4ce876a774c95f66
Bishesh Shrestha <bisheshshr@gmail.com>
2024-04-04 16:36:03 -0500

    Changed the log.txt to log.log

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -11,7 +11,7 @@ const fs = require("fs");
 const { debugPort } = require("process");
 const { use } = import("chai");
 const PORT = 3000;
-const activity_log_file = "log.txt";
+const activity_log_file = "log.log";
 const current_time = new Date();
 
 const db = sqlite("database.db");
