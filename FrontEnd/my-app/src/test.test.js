ee9861800dc7837202593596d7e7c0963cc089a1
Brady Petterson <119450374+Bradyp34@users.noreply.github.com>
2024-04-04 15:22:00 -0500

    rebuilt dependencies and updated some testing criterea

--- FrontEnd/my-app/src/test.test.js
+++ FrontEnd/my-app/src/test.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 // Import components. Adjust the paths according to your project structure.
@@ -15,15 +15,37 @@ describe('React Component Render Tests', () => {
         expect(screen.getByRole('button', { name: /Login/i })).toBeInTheDocument();
     });
 
+    test('LoginPage allows the user to toggle password visibility', async () => {
+        render(<LoginPage />);
+
+        // Initial check: password should be hidden
+        const passwordInput = screen.getByLabelText('Password');
+        expect(passwordInput).toHaveAttribute('type', 'password');
+
+        // Click the toggle visibility button
+        const toggleButton = screen.getByText('Show'); // Assuming the button initially says "Show"
+        fireEvent.click(toggleButton);
+
+        // Now, the password should be visible
+        expect(passwordInput).toHaveAttribute('type', 'text');
+
+        // Click again to hide the password
+        const hideButton = screen.getByText('Hide'); // The button should now say "Hide"
+        fireEvent.click(hideButton);
+
+        // The password should be hidden again
+        expect(passwordInput).toHaveAttribute('type', 'password');
+    });
+
     test('ItemSearchPage should render without crashing', () => {
         render(<ItemSearchPage />);
-        // Tests if page loads by searching for "Account Page" text on the page
+        // Tests if page loads by searching for "Search" button on the page
         expect(screen.getByRole('button', { name: /Search/i })).toBeInTheDocument();
     });
 
     test('InventoryPage should render without crashing', () => {
         render(<InventoryPage />);
-        // Tests if page loads by searching for "Account Page" text on the page
+        // Tests if page loads by searching for "Inventory Page" text on the page
         expect(screen.getByText(/Inventory Page/i)).toBeInTheDocument();
     });
 
