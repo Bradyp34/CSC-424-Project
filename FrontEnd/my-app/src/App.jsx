de02ab2eb2b5f25f82b5f7736e2e3f3c3f711976
HotBunny <ehmoo924@gmail.com>
2024-04-05 02:12:50 -0500

    main page

--- FrontEnd/my-app/src/App.jsx
+++ FrontEnd/my-app/src/App.jsx
@@ -8,8 +8,12 @@ function App() {
   return (
    <div>
     <Navbar/>
+    <div className='grid place-items-center h-screen bg-gray-900 text-white'>
+        <h1 className='text-3xl font-bold'>Welcome to the main page!</h1>
+        </div>
    </div>
   );
 }
 
 export default App;
+
