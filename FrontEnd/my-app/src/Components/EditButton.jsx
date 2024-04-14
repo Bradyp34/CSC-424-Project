bb48eacd604dc448652aec508f3c081f51553d2a
HotBunny <ehmoo924@gmail.com>
2024-04-14 14:47:18 -0500

    style edit button

--- FrontEnd/my-app/src/Components/EditButton.jsx
+++ FrontEnd/my-app/src/Components/EditButton.jsx
@@ -10,7 +10,13 @@ function EditButton({ productId }) {
 
     return (
         <div>
-            {<button onClick={handleClick}>Edit</button>}
+            {
+            
+            <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Edit
+            </button>
+            
+            }
         </div>
     );
 }
